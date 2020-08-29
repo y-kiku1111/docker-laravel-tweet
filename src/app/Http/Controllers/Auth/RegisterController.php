9610de0b411c10972287c2bdeb89f9a9a007b00b
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-08-29 18:47:57 +0900

    “photo”

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            // 追加
+            'screen_name' => ['required', 'string', 'max:255', 'unique:users'],
+            'name'        => ['required', 'string', 'max:255'],
+            'email'       => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password'    => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
@@ -64,6 +66,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            // 追加
+            'screen_name' => $data['screen_name'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
